719f9a0604c55ba675cb759c1bcd0b89dbfc3b58
y19th-mac <whenmistdisappear@gmail.com>
2024-12-18 23:54:47 +0300

    ios fixes

--- core/domain/src/commonMain/kotlin/com/volsu/unijournal/core/domain/mapper/Subject.kt
+++ core/domain/src/commonMain/kotlin/com/volsu/unijournal/core/domain/mapper/Subject.kt
@@ -4,27 +4,29 @@ import com.volsu.unijournal.core.domain.models.Subject
 import com.volsu.unijournal.core.domain.models.SubjectForm
 import com.volsu.unijournal.core.local.entities.subjects.SubjectEntity
 import com.volsu.unijournal.core.local.entities.subjects.SubjectType
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
-fun SubjectEntity.toSubjectModel() = Subject(
+fun SubjectEntity.toSubjectModel(): Subject = Subject(
     name = name,
     type = type.toSubjectForm()
 )
 
-fun List<SubjectEntity>.toImmutableSubjectsList() = map { it.toSubjectModel() }.toImmutableList()
+fun List<SubjectEntity>.toImmutableSubjectsList(): ImmutableList<Subject> =
+    map { it.toSubjectModel() }.toImmutableList()
 
-fun Subject.toSubjectEntity() = SubjectEntity(
+fun Subject.toSubjectEntity(): SubjectEntity = SubjectEntity(
     name = name,
     type = type.toSubjectType()
 )
 
-fun SubjectType.toSubjectForm() = when(this) {
+fun SubjectType.toSubjectForm(): SubjectForm = when (this) {
     SubjectType.Laboratory -> SubjectForm.Laboratory
     SubjectType.Lecture -> SubjectForm.Lecture
     SubjectType.Seminar -> SubjectForm.Seminar
 }
 
-fun SubjectForm.toSubjectType() = when(this) {
+fun SubjectForm.toSubjectType(): SubjectType = when (this) {
     SubjectForm.Laboratory -> SubjectType.Laboratory
     SubjectForm.Lecture -> SubjectType.Lecture
     SubjectForm.Seminar -> SubjectType.Seminar
